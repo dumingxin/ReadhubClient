fecee3e3f9148653150ea2e487cc39e03145a13a
dmx <mail@dmx.name>
2017-11-02 11:21:49 +0800

    添加侧滑菜单

--- app/src/main/java/name/dmx/readhubclient/MainActivity.kt
+++ app/src/main/java/name/dmx/readhubclient/MainActivity.kt
@@ -1,21 +1,28 @@
 package name.dmx.readhubclient
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import name.dmx.readhubclient.adapter.TabFragmentAdapter
 import name.dmx.readhubclient.fragment.NewsFragment
-import name.dmx.readhubclient.fragment.NotYetImplementedFragment
 import name.dmx.readhubclient.fragment.TechNewsFragment
 import name.dmx.readhubclient.fragment.TopicFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         toolbar.setTitle(R.string.app_name)
+        val actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(actionBarToggle)
+        navigationView.setNavigationItemSelectedListener(this)
+        actionBarToggle.syncState()
         init()
     }
 
@@ -27,8 +34,13 @@ class MainActivity : AppCompatActivity() {
         tabLayout.setupWithViewPager(viewPager)
     }
 
-    override fun onStart() {
-        super.onStart()
-
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.title -> {
+            }
+            else -> Snackbar.make(toolbar, item!!.title, Snackbar.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawers()
+        return true
     }
 }
